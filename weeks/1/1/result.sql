b29e9877b543cc4e4c62df73948607a89f7529ee
Ohdaejin <xla3095@gmail.com>
2021-12-21 10:14:59 +0900

    db_수정1

--- weeks/1/1/result.sql
+++ weeks/1/1/result.sql
@@ -4,7 +4,7 @@ USE mega_shop;
 
 -- 1. 마켓
 CREATE TABLE `markets_market`( 
-    `market_id` INT(100) NOT NULL AUTO_INCREMENT PRIMARY KEY, -- 연결된 업체번호
+    id INT(100) NOT NULL AUTO_INCREMENT PRIMARY KEY, -- 연결된 업체번호
     `market_name` VARCHAR(30) NOT NULL, -- 연결된 업체 이름
     `market_phone_number` CHAR(100) NOT NULL, -- 연결된 업체 전화번호
     `market_adress` TEXT(100) NOT NULL, -- 연결된 업체 주소
@@ -13,14 +13,14 @@ CREATE TABLE `markets_market`(
 
 -- 1-1. 데이터
 INSERT INTO `markets_market`
-SET `market_id` = '1',
+SET id = '1',
 `market_name` = '옷가게1',
 `market_phone_number` = '01012345678',
 `market_adress` = '서울',
 `market_account` = '김김김';
 
 INSERT INTO `markets_market`
-SET `market_id` = '2',
+SET id = '2',
 `market_name` = '옷가게2',
 `market_phone_number` = '01012345699',
 `market_adress` = '대전',
@@ -39,6 +39,7 @@ CREATE TABLE `products_product`(
   `sold_out_status` BOOLEAN, -- 품절 미품절
   `market_id` INT(100) -- 상품 소속 마켓 번호
 );
+
 -- 2-1. 데이터
 INSERT INTO `products_product`
 SET regDate = NOW(),
@@ -64,24 +65,27 @@ SET regDate = NOW(),
 
 -- 3. 상품옵션
 CREATE TABLE `products_product_opt`(
-  `ppo_id` INT(100) NOT NULL AUTO_INCREMENT PRIMARY KEY, -- 상품번호
-  `ppo_size` CHAR(10) NOT NULL, -- 상품 사이즈
-  `ppo_color` CHAR(10) NOT NULL, -- 상품 색
-  `ppo_rest` INT(100) NOT NULL -- 남은 상품
+  `opt_id` INT(100) NOT NULL AUTO_INCREMENT PRIMARY KEY, -- 상품옵션고유번호
+  `product_id` INT(100) NOT NULL, -- 상품 번호
+  `opt_size` CHAR(10) NOT NULL, -- 상품 사이즈
+  `opt_color` CHAR(10) NOT NULL, -- 상품 색
+  `opt_rest` INT(100) NOT NULL -- 남은 상품
 );
 
 -- 3-1. 데이터 
 INSERT INTO `products_product_opt`
-SET `ppo_id` = 1,
-`ppo_size` = 'R',
-`ppo_color` = 'RED',
-`ppo_rest` = 3;
+SET `opt_id` = 1,
+`product_id` = 1,
+`opt_size` = 'R',
+`opt_color` = 'RED',
+`opt_rest` = 3;
 
 INSERT INTO `products_product_opt`
-SET `ppo_id` = 2,
-`ppo_size` = 'S',
-`ppo_color` = 'BLACK',
-`ppo_rest` = 2;
+SET `opt_id` = 2,
+`product_id` = 2,
+`opt_size` = 'S',
+`opt_color` = 'BLACK',
+`opt_rest` = 2;
 
 -- 4. 유저정보
 CREATE TABLE `account_user`(
@@ -115,7 +119,8 @@ CREATE TABLE `cart_cart_item`(
   `product_id` INT(100) NOT NULL AUTO_INCREMENT PRIMARY KEY, -- 상품 번호
   `product_name` VARCHAR(30) NOT NULL, -- 상품 이름
   `product_color` VARCHAR(30) NOT NULL, -- 상품 색상
-  `product_size` VARCHAR(30) NOT NULL -- 상품 사이즈
+  `product_size` VARCHAR(30) NOT NULL, -- 상품 사이즈
+  `product_pack_number` INT(100) NOT NULL -- 상품 담은 수량
   );
 
 -- 5-1. 데이터
@@ -123,10 +128,12 @@ INSERT INTO `cart_cart_item`
 SET `product_id` = 1,
 `product_name` = '무슨무슨 패딩',
 `product_color` = 'RED',
-`product_size` = 'L';
+`product_size` = 'L',
+`product_pack_number` = 1;
 
 INSERT INTO `cart_cart_item`
 SET `product_id` = 2,
 `product_name` = '무슨무슨 재킷',
 `product_color` = 'BLACK',
-`product_size` = 'S';
+`product_size` = 'S',
+`product_pack_number` = 2;
