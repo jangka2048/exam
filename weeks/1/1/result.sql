b6bf97bcdced184c22a11be330920f094befdbe1
rlaalsrbaa <rlaalsrbaa@gmail.com>
2021-12-21 10:16:32 +0900

    장바구니 수량 필드와 QNA 테이블 생성

--- weeks/1/1/result.sql
+++ weeks/1/1/result.sql
@@ -1,7 +1,7 @@
 DROP DATABASE IF EXISTS mega_shop;
 CREATE DATABASE mega_shop;
 USE mega_shop;
-
+-- 마켓
 CREATE TABLE markets_market(
 id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
 reg_date DATETIME NOT NULL,
@@ -20,6 +20,7 @@ INSERT INTO markets_market
 SET reg_date = NOW(),
 `name` = "11번가";
 
+-- 상품
 CREATE TABLE products_product(
 id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
 market_id INT(10) UNSIGNED NOT NULL,
@@ -61,6 +62,7 @@ SET update_date = NOW(),
 sale_price = 145000
 WHERE id = 1;
 
+-- 상품옵션
 CREATE TABLE products_product_opt(
 id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
 product_id INT(10) UNSIGNED NOT NULL,
@@ -97,6 +99,7 @@ color = "green",
 size = "105",
 opt_price = 5000;
 
+-- 회원정보
 CREATE TABLE account_user(
 id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
 login_id VARCHAR(100) NOT NULL,
@@ -135,26 +138,98 @@ nickname = "dogcat77",
 lastname = "임",
 adress = "김해시 맹구";
 
+-- 장바구니
 CREATE TABLE cart_cart_item(
 id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
 product_id INT(10) UNSIGNED NOT NULL,
 product_opt_id INT(10) UNSIGNED NOT NULL,
-sale_price INT(10) UNSIGNED NOT NULL
+sale_price INT(10) UNSIGNED NOT NULL,
+dibs INT(10) UNSIGNED NOT NULL
 );
 
 INSERT INTO cart_cart_item
 SET product_id = 1,
 product_opt_id = 3,
-sale_price = 150000;
-
+sale_price = 150000,
+dibs = 1;
 
 INSERT INTO cart_cart_item
 SET product_id = 2,
 product_opt_id = 4,
-sale_price = 60000;
+sale_price = 60000,
+dibs = 1;
 
 
 INSERT INTO cart_cart_item
 SET product_id = 3,
 product_opt_id = 5,
-sale_price = 25000;
+sale_price = 25000,
+dibs = 1;
+
+-- QNA question
+CREATE TABLE QNA_question(
+id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+user_id INT(10) UNSIGNED NOT NULL,
+reg_date DATETIME,
+answer_status BOOLEAN NOT NULL,
+question_subject VARCHAR(100) NOT NULL,
+question_content TEXT NOT NULL
+);
+
+INSERT INTO QNA_question
+SET user_id = 1,
+reg_date = NOW(),
+question_subject = "오늘 점심은 뭔가요?",
+question_content = "점심메뉴가 너무 궁금합니다.";
+
+INSERT INTO QNA_question
+SET user_id = 2,
+reg_date = NOW(),
+question_subject = "안녕하세요",
+question_content = "반갑습니다.";
+
+INSERT INTO QNA_question
+SET user_id = 3,
+reg_date = NOW(),
+question_subject = "환불기한은 언제까지인가요?",
+question_content = "감사합니다.";
+
+-- QNA answer
+CREATE TABLE QNA_answer(
+id INT(10) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+user_id INT(10) UNSIGNED NOT NULL,
+reg_date DATETIME,
+question_id INT(10) UNSIGNED NOT NULL,
+answer_content TEXT NOT NULL
+);
+
+INSERT INTO QNA_answer
+SET user_id = 1,
+reg_date = NOW(),
+question_id = 1,
+answer_content = "순대국밥입니다.";
+
+UPDATE QNA_question
+SET answer_status = 1
+WHERE id = 1;
+
+INSERT INTO QNA_answer
+SET user_id = 1,
+reg_date = NOW(),
+question_id = 2,
+answer_content = "반가워요.";
+
+UPDATE QNA_question
+SET answer_status = 1
+WHERE id = 2;
+
+
+INSERT INTO QNA_answer
+SET user_id = 1,
+reg_date = NOW(),
+question_id = 3,
+answer_content = "일주일 입니다.";
+
+UPDATE QNA_question
+SET answer_status = 1
+WHERE id = 3;
